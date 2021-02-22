946a2f22a8667d1f28db9283f35ee3b15d380e36
Alan Gardner <urfolomeus@gmail.com>
2021-02-22 08:15:50 +0000

    Adding poetry path

--- zinit/config/env.zsh
+++ zinit/config/env.zsh
@@ -1,5 +1,9 @@
 # user local bin and share
 export PATH="${HOME}/.local/bin:${HOME}/.local/share:$PATH"
 
+# poetry
+export PATH="$HOME/.poetry/bin:$PATH"
+
 # yarn
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
