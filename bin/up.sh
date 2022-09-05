89eb603ee82f207d1c8ee11d1481e42284f21cd9
Alan Gardner <urfolomeus@gmail.com>
2022-09-05 12:18:43 +0100

    Fixing bug in up script

--- bin/up.sh
+++ bin/up.sh
@@ -5,27 +5,27 @@ echo "Current working directory: $PWD\n\n"
 
 
 # asdf
-
 echo "Setting up asdf..."
 ln -s $PWD/asdf/.asdfrc $HOME/.asdfrc
 
-# fish
 
-echo "Setting up fish..."
-source $PWD/fish/fish_up.sh
+# fish
+cd $PWD/fish
+source fish_up.sh
+cd ..
+
 
 # git
-
 echo "Setting up git..."
 ln -s $PWD/git/.gitignore $HOME/.gitignore
 
-# ruby
 
+# ruby
 echo "Setting up gemrc..."
 ln -s $PWD/ruby/.gemrc $HOME/.gemrc
 
-# tmux
 
+# tmux
 echo "Setting up tmux..."
 ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 
