d21d74b88f373b5c095e2c605917920bcbce6dc2
Alan Gardner <urfolomeus@gmail.com>
2022-09-05 12:50:53 +0100

    Adjust fish up script to symlink rather than copy

--- fish/fish_up.sh
+++ fish/fish_up.sh
@@ -3,9 +3,13 @@
 echo "Setting up fish..."
 mkdir -p $HOME/.config/fish
 
-cp -r $PWD/config/* $HOME/.config/fish
-cp -r $PWD/functions $HOME/.config/fish
+ln -s $PWD/config/config.fish $HOME/.config/fish/config.fish
+ln -s $PWD/config/aliases.fish $HOME/.config/fish/aliases.fish
+ln -s $PWD/config/linux $HOME/.config/fish/linux
+ln -s $PWD/config/mac $HOME/.config/fish/mac
 
 echo "Setting up fisher..."
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install pure-fish/pure
+
+ln -s $PWD/functions/mcd.fish $HOME/.config/fish/functions/mcd.fish
