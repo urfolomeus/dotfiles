284f9595a07ba3ba907e7687e27b6a606490bbd2
Alan Gardner <urfolomeus@gmail.com>
2024-10-30 17:38:19 +0000

    Moving the config file into the root

---
+++ fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source ~/.config/fish/config/aliases.fish
+source ~/.config/fish/config/env.fish
+
+switch (uname)
+case Linux
+    source ~/.config/fish/config/linux/env.fish
+    source ~/.config/fish/config/linux/system.fish
+case Darwin
+    source ~/.config/fish/config/mac/env.fish
+    source ~/.config/fish/config/mac/system.fish
+case '*'
+    echo `Do not know about (uname)`
+end
