b364e120010981dee6716736c33291ab6b3b7af6
Alan Gardner <urfolomeus@gmail.com>
2024-10-30 22:35:19 +0000

    Fish env file updates

--- fish/config/mac/env.fish
+++ fish/config/mac/env.fish
@@ -1,8 +1,9 @@
 # PATH
 fish_add_path /opt/homebrew/bin
-fish_add_path $HOME/.asdf/bin
+fish_add_path $HOME/.asdf/shims
 fish_add_path $HOME/.local/bin
 
 
 # ENV VARS
-set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/opt/homebrew/opt/openssl@1.1"
+# set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=/opt/homebrew/opt/openssl@1.1"
+set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
