b894d7ee472be10800d21467bac25f4c4452b05c
Alan Gardner <urfolomeus@gmail.com>
2024-11-19 11:46:01 +0000

    Only load fish prefs in an interactive terminal

--- fish/config.fish
+++ fish/config.fish
@@ -1,30 +1,29 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Common config
+  source ~/.config/fish/config/common/aliases.fish
+  source ~/.config/fish/config/common/env.fish
+  source ~/.config/fish/config/common/prompt.fish
+  source ~/.config/fish/config/common/system.fish
+
+
+  # OS-specific config
+  switch (uname)
+  case Linux
+      if check_wsl then
+    source ~/.config/fish/config/wsl/aliases.fish
+    source ~/.config/fish/config/wsl/env.fish
+    source ~/.config/fish/config/wsl/system.fish
+      else
+          source ~/.config/fish/config/linux/aliases.fish
+    source ~/.config/fish/config/linux/env.fish
+    source ~/.config/fish/config/linux/system.fish
+      end
+  case Darwin
+      source ~/.config/fish/config/mac/aliases.fish
+      source ~/.config/fish/config/mac/env.fish
+      source ~/.config/fish/config/mac/system.fish
+  case '*'
+      echo `Do not know about (uname)`
+  end
 end
 
-# Common config
-source ~/.config/fish/config/common/aliases.fish
-source ~/.config/fish/config/common/env.fish
-source ~/.config/fish/config/common/prompt.fish
-source ~/.config/fish/config/common/system.fish
-
-
-# OS-specific config
-switch (uname)
-case Linux
-    if check_wsl then
-	source ~/.config/fish/config/wsl/aliases.fish
-	source ~/.config/fish/config/wsl/env.fish
-	source ~/.config/fish/config/wsl/system.fish
-    else
-        source ~/.config/fish/config/linux/aliases.fish
-	source ~/.config/fish/config/linux/env.fish
-	source ~/.config/fish/config/linux/system.fish
-    end
-case Darwin
-    source ~/.config/fish/config/mac/aliases.fish
-    source ~/.config/fish/config/mac/env.fish
-    source ~/.config/fish/config/mac/system.fish
-case '*'
-    echo `Do not know about (uname)`
-end
