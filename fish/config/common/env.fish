f9350acfc6e8b364646676a6cd8aa543c823f4ac
Alan Gardner <urfolomeus@gmail.com>
2025-09-21 09:07:18 +0100

    Adding some custom command line tools

--- fish/config/common/env.fish
+++ fish/config/common/env.fish
@@ -4,3 +4,4 @@ set -gx fish_greeting "🤖 Smiley day to ya!"
 
 # PATH
 fish_add_path $HOME/.asdf/shims
+fish_add_path $HOME/src/personal/scripts/typescript/omglol-cli/bin
--- fish/config/mac/env.fish
+++ fish/config/mac/env.fish
@@ -1,6 +1,7 @@
 # PATH
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/src/tools/terravision
 
 
 # ENV VARS
